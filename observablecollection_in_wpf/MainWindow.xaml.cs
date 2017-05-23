15899c3e9052227f27b8b90ce644004bcd795f43
atma0004 <atma0004@edu.eal.dk>
2017-05-23 12:51:01 +0200

    Add project files.

---
+++ observablecollection_in_wpf/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace observablecollection_in_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //List<Player> Players = new List<Player>();
+        ObservableCollection<Player> Players = new ObservableCollection<Player>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this.GetPlayer();
+        }
+        //public List<Player> GetPlayer()
+        public ObservableCollection<Player> GetPlayer()
+        {
+            Players.Add(new Player() { Id = 1, Name = "atmar" });
+            return Players;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Players.Add(new Player() { Id = 4, Name = "ahmad" });
+        }
+    }
+}
---
+++ observablecollection_in_wpf/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace observablecollection_in_wpf
+{
+    //  1 first we make this class public
+    public class Player
+    {
+        #region
+        /* the role of observable collection in wpf is for maintaining collecttion of objects in winforms ,web or console applications 
+         if have to make a list of students you should make a list of students or genric list of student so this case does not work in 
+         wpf because wpf 'UI constantly needs notifycation from code behind or from your code
+         */
+        #endregion
+        // 2 lets add two properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // 3  lets go the code behind we are not making MVVM for this demo
+    }
+}
